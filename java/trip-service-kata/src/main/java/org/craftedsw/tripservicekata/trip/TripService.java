28fb51992330038cd65e7e7252b3cb9427cbfb7f
Sumanth Pola <sumanth.pola@zemosolabs.com>
2022-06-16 17:01:48 +0530

    Modified TripService class to implement solid principles and adding the test class for TripService class

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,23 +10,36 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
+		User loggedUser = getLoggedUser();
+		isUserLoggedIn(loggedUser);
+		return getFriendList(user,  loggedUser);
 	}
-	
+
+	private void isUserLoggedIn(User loggedUser) throws UserNotLoggedInException{
+		if (loggedUser==null)
+			throw new UserNotLoggedInException();
+	}
+
+	private List<Trip> getFriendList(User user, User loggedUser) {
+		boolean isFriend = false;
+		for (User friend : user.getFriends()) {
+			if (friend.equals(loggedUser)) {
+				isFriend = true;
+				break;
+			}
+		}
+		if (isFriend) {
+			return getTripList(user);
+		}
+		return new ArrayList<>();
+	}
+
+	protected List<Trip> getTripList(User user) {
+		return TripDAO.findTripsByUser(user);
+	}
+
+	protected User getLoggedUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,5 +1,56 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import java.util.List;
+
 public class TripServiceShould {
-	
+    private User loggedUser = new User();
+    private User notLoggedUser = new User();
+    private User anotherUser = new User();
+    private TripService tripService = new TripService(){
+
+        @Override
+        protected List<Trip> getTripList(User user) {
+            return user.trips();
+        }
+
+        @Override
+        protected User getLoggedUser() {
+            return loggedUser;
+        }
+    };
+
+    @Before
+    public void beforeClass(){
+//        tripService =
+    }
+	@Test(expected = UserNotLoggedInException.class)
+    public void testGetTripsByUserForNoLoggedInUser(){
+        loggedUser = null;
+        tripService.getTripsByUser(anotherUser);
+    }
+
+    @Test
+    public void testGetTripByUserForLoggedInUser(){
+        tripService.getTripsByUser(notLoggedUser);
+    }
+
+    @Test
+    public void testGetTripByUserLoggedInAndFriend(){
+        anotherUser.addFriend(loggedUser);
+        anotherUser.addTrip(new Trip());
+        anotherUser.addTrip(new Trip());
+        assertEquals(2, tripService.getTripsByUser(anotherUser).size());
+    }
+
+    @Test
+    public void testGetTripByUserLoggedInAndNotFriend(){
+        anotherUser.addFriend(new User());
+        assertEquals(0, tripService.getTripsByUser(anotherUser).size());
+    }
 }
